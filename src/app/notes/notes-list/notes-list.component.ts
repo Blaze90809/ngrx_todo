192881c31897c359d3606b61f1a737b73a0828c1
Blaze Fugina <blaze.fugina@gmail.com>
2018-11-01 23:50:11 -0500

    Moved list functionality to list view.

--- src/app/notes/notes-list/notes-list.component.ts
+++ src/app/notes/notes-list/notes-list.component.ts
@@ -1,4 +1,11 @@
+import { GET_TODO } from './../../store/todo.action';
+import { ToDoState } from './../../store/todo.state';
 import { Component, OnInit } from '@angular/core';
+import { Store, select, Action } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+
+import ToDo from '../../todo.model';
 
 @Component({
   selector: 'app-notes-list',
@@ -7,9 +14,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<ToDoState>) { }
 
   ngOnInit() {
+    this.ToDoState$ = this.store.pipe(select('todos'));
+
+    let getTodoAction: Action = {
+      type: GET_TODO
+    }
+    this.ToDoSubscription = this.ToDoState$.pipe(map(x => this.ToDoList = x.ToDoList)).subscribe();
+    this.store.dispatch(getTodoAction);
+  }
+  ToDoState$: Observable<ToDoState>;
+  ToDoSubscription: Subscription;
+  ToDoList: ToDo[];
+
+  ngOnDestroy() {
+    this.ToDoSubscription.unsubscribe();
   }
 
 }
